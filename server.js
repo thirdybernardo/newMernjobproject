0df886c6e73cf3f014ad941ad1bbd6dc10c2c1cd
bernardl24 <thirdybernardo2410@gmail.com>
2024-05-14 16:04:21 +0800

    Test

--- server.js
+++ server.js
@@ -33,16 +33,12 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.static(path.resolve(__dirname, '/client/dist')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
 app.use(mongoSanitize());
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
 app.get('/api/v1/test', (req, res) => {
   res.json({ msg: 'test route' });
@@ -53,7 +49,7 @@ app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/client/dist/index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist/index.html'));
 });
 
 app.use('*', (req, res) => {
